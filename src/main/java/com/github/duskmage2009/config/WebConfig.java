8d7816a1c75d54886003d5a757060e7ed7773b79
Kirill <Duskmage2009@gmai.com>
2025-12-27 14:59:37 +0200

    "Added Web config"

---
+++ src/main/java/com/github/duskmage2009/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.github.duskmage2009.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3050")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Content-Disposition");
+
+    }
+
+    }
+
